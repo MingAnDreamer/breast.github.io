8fc8019672bda74b46e1e4d623817ad18e6fe08c
MingAnDreamer <3544570467@qq.com>
2026-03-20 17:38:18 +0800

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/breast.github.io/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
