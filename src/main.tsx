6cceb3d0cd9cdb087fe8150831ffc4ccead2066b
MingAnDreamer <3544570467@qq.com>
2026-03-20 18:04:54 +0800

    Refactor main.tsx to use BrowserRouter and StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { HashRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter basename="/breast.github.io">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
